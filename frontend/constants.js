9e6ca851fd224e423507344b56ee5fcd72fe639e
dhxpoll <danielhilfer@icloud.com>
2022-08-15 20:06:23 +0200

    Start

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+    "https://api.thegraph.com/subgraphs/name/dhxpoll/celo-nft-marketplace";
+
+export const MARKETPLACE_ADDRESS = "0xb1d9b8D5410704F1811523fad07B83763dC472b3";
